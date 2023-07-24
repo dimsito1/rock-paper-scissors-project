7dd5f4e557f9ae5eb5031a57297268df2e20fd9a
Dimislav <dimivelkov1@gmail.com>
2023-07-24 20:29:48 +0300

    visible scoring added

--- main.js
+++ main.js
@@ -1,95 +1,99 @@
 const Choices = ["rock", "paper", "scissors"];
 
+
+let gameOver= false;
 let playerPoints = 0;
 let computerPoints = 0;
+let playerChoice;
+
+let playerScoreElement = document.querySelector(".player-score");
+let computerScoreElement = document.querySelector(".computer-score");
+
+const rockChoice = document.querySelector("#rock-choice");
+const paperChoice = document.querySelector("#paper-choice");
+const scissorsChoice = document.querySelector("#scissors-choice");
 
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * 3);
     return Choices[randomIndex];
 }
 
-function getPlayerChoice() {
-    const playerChoice = prompt("Enter rock, paper or scissors:");
-    if (playerChoice.toLowerCase() !== 'rock' && playerChoice.toLowerCase() !== 'paper' && playerChoice.toLowerCase() !== 'scissors') {
-        alert('Please enter \"rock\", \"paper\" or \"scissors\"');
-        return getPlayerChoice();
+// function getPlayerChoice() {
+//     const playerChoice = prompt("Enter rock, paper or scissors:");
+//     if (playerChoice.toLowerCase() !== 'rock' && playerChoice.toLowerCase() !== 'paper' && playerChoice.toLowerCase() !== 'scissors') {
+//         alert('Please enter \"rock\", \"paper\" or \"scissors\"');
+//         return getPlayerChoice();
+//     }
+//     return playerChoice.toLowerCase();
+// }
+
+rockChoice.addEventListener("click", function() {
+    console.log("rock");
+    playerChoice = "rock";
+    playerRound();
+});
+paperChoice.addEventListener("click", function() {
+    console.log("paper");
+    playerChoice = "paper";
+    playerRound();
+});
+scissorsChoice.addEventListener("click", function() {
+    console.log("scissors");
+    playerChoice = "paper";
+    playerRound();
+});
+
+function playerRound() {
+    if (gameOver) {
+        console.log("game has finished");
+        return;
     }
-    return playerChoice.toLowerCase();
+    let computerChoice = getComputerChoice();
+    console.log(`computer choice: ${computerChoice}`);
+
+    let result = singleRound(playerChoice, computerChoice);
+
+    if (result === 'win') {
+        playerPoints++;
+        playerScoreElement.textContent = playerPoints;
+    }
+    else if (result === 'lose') {
+        computerPoints++;
+        computerScoreElement.textContent = computerPoints;
+    }
+    console.log(`Player points: ${playerPoints}, Computer points: ${computerPoints}`);
+
+    game();
 }
 
 function singleRound(playerSelection, computerSelection) {
-    console.log(`computerChoice: ${computerSelection}`);
-    console.log(`playerChoice: ${playerSelection.toLowerCase()}`);
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'rock') {
-            console.log('Draw.');``
-            return 'draw';
-        }
-        else if (computerSelection === 'paper') {
-            console.log('You lost.');
-            return 'lose';
-        }
-        else {
-            console.log('You won!');
-            return 'win';
-        }
+    if (playerSelection === computerSelection) {
+        console.log("draw");
+        return 'draw';
     }
-    else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            console.log('You won!');
-            return 'win';
-        }
-        else if (computerSelection === 'paper') {
-            console.log('Draw.');
-            return 'draw';
-        }
-        else {
-            console.log('You lost.');
-            return 'lose';
-        }
+    else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerChoice === 'paper')
+    ) {
+        console.log('You won!');
+        return 'win';
     }
     else {
-        if (computerSelection === 'rock') {
-            console.log('You lost.');
-            return 'lose';
-        }
-        else if (computerSelection === 'paper') {
-            console.log('You won!');
-            return 'win';
-        }
-        else {
-            console.log('Draw.');
-            return 'draw';
-        }
+        console.log('You lost.');
+        return 'lose';
     }
 }
 
 function game() {
-    let getResult;
-
-    for (let i = 0; i < 100; i++) {
-        if (playerPoints - computerPoints == 3 || playerPoints == 5) {
-            console.log('YOU WIN!');
-            return;
-        }
-        else if (computerPoints - playerPoints == 3 || computerPoints == 5) {
-            console.log('you lost.');
-            return        
-        }
-        
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        
-        getResult = singleRound(playerChoice, computerChoice);
-        if (getResult === 'win') {
-            playerPoints++;
-            console.log(`player points: ${playerPoints}`);
-        }
-        else if (getResult === 'lose') {
-            computerPoints++;
-            console.log(`computer points: ${computerPoints}`);
-        }
+    if (playerPoints - computerPoints == 3 || playerPoints == 5) {
+        console.log('YOU WIN!');
+        gameOver = true;
+        return;
+    }
+    else if (computerPoints - playerPoints == 3 || computerPoints == 5) {
+        console.log('you lost.');
+        gameOver = true;
+        return;
     }
 }
-
-//game();
