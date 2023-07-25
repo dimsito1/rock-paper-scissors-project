a402488fb9357c70a49ec30fc1b6bebabd25b0b7
Dimislav <dimivelkov1@gmail.com>
2023-07-25 15:41:11 +0300

    site finish

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const Choices = ["rock", "paper", "scissors"];
 
-
+let playerWon = false;
 let gameOver= false;
 let playerPoints = 0;
 let computerPoints = 0;
+
+let computerChoice;
 let playerChoice;
 
+const rockImage = document.querySelector('.rock-image');
+
+const middleContainer = document.querySelector('.middle-container');
 let playerScoreElement = document.querySelector(".player-score");
 let computerScoreElement = document.querySelector(".computer-score");
 
@@ -27,28 +32,97 @@ function getComputerChoice() {
 //     return playerChoice.toLowerCase();
 // }
 
+function playing() {
+    middleContainer.classList.remove('middle-container');
+    middleContainer.classList.add('playing');
+    middleContainer.innerHTML = `
+<div class="player-playing">
+    <p>
+        Your choice:
+    </p>
+    <img src="./images/${playerChoice}.png" alt="player-choice-image">
+</div>
+<div class="computer-playing">
+<p>
+Robot's choice:
+</p>
+<img src="./images/${computerChoice}.png" alt="computer-choice-image">
+</div>`
+}
+
+function getGameOver(playerWonBool) {
+    const mainContainer = document.querySelector('body');
+    if (playerWonBool) {
+        mainContainer.innerHTML = `
+        <div class="winning-screen">
+        <p class="animation-text">
+        YOU WON.
+        </p>
+        <button class="try-again">
+        Try again.
+        </button>
+        </div>`
+    }
+    else {
+        mainContainer.innerHTML = `
+        <div class="losing-screen">
+        <p class="animation-text">
+        YOU LOST.
+        </p>
+        <button class="try-again">
+        Try again.
+        </button>
+        </div>`
+    }
+    
+    const tryAgain = document.querySelector('.try-again');
+    
+    tryAgain.addEventListener('click', function() {
+        console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+        playerWon = false;
+        gameOver = false;
+        playerPoints = 0;
+        computerPoints = 0;
+    
+        // Refresh the page
+        location.reload();
+    });
+}
+
 rockChoice.addEventListener("click", function() {
-    console.log("rock");
+    if (gameOver) {
+        console.log("game has finished");
+        getGameOver(gameOver);
+        return;
+    }
     playerChoice = "rock";
+    console.log("rock");
     playerRound();
+    playing();
 });
 paperChoice.addEventListener("click", function() {
-    console.log("paper");
-    playerChoice = "paper";
-    playerRound();
-});
-scissorsChoice.addEventListener("click", function() {
-    console.log("scissors");
-    playerChoice = "paper";
-    playerRound();
-});
-
-function playerRound() {
     if (gameOver) {
         console.log("game has finished");
         return;
     }
-    let computerChoice = getComputerChoice();
+    console.log("paper");
+    playerChoice = "paper";
+    playerRound();
+    playing();
+});
+scissorsChoice.addEventListener("click", function() {
+    if (gameOver) {
+        console.log("game has finished");
+        return;
+    }
+    console.log("scissors");
+    playerChoice = "scissors";
+    playerRound();
+    playing();
+});
+
+function playerRound() {
+    computerChoice = getComputerChoice();
     console.log(`computer choice: ${computerChoice}`);
 
     let result = singleRound(playerChoice, computerChoice);
@@ -88,12 +162,18 @@ function singleRound(playerSelection, computerSelection) {
 function game() {
     if (playerPoints - computerPoints == 3 || playerPoints == 5) {
         console.log('YOU WIN!');
+        playerWon = true;
         gameOver = true;
-        return;
+        setTimeout(() => {
+            getGameOver(playerWon);
+        }, 700);
     }
     else if (computerPoints - playerPoints == 3 || computerPoints == 5) {
         console.log('you lost.');
+        playerWon = false;
         gameOver = true;
-        return;
+        setTimeout(() => {
+            getGameOver(playerWon);
+        }, 700);
     }
 }
